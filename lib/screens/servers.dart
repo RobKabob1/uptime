bfc72c51eccaf99e29c56e778d0b43f00f012553
rwp005 <rwp005@gmail.com>
2023-02-28 11:42:30 -0600

    Created an Animated List and added a Floating Action Button

--- lib/screens/servers.dart
+++ lib/screens/servers.dart
@@ -8,31 +8,100 @@ class Servers extends StatefulWidget {
 }
 
 class _ServersState extends State<Servers> {
+  final _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+
+  void _addItem() {
+    _items.insert(0, "Server ${_items.length + 1}");
+    _key.currentState!.insertItem(
+      0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _removeItem(int index) {
+    _key.currentState!.removeItem(
+      index,
+      (_, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: const ListTile(
+            title: Text(
+              "Deleted",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        );
+      },
+      duration: const Duration(milliseconds: 300),
+    );
+    _items.removeAt(index);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-        padding: const EdgeInsets.all(9),
-        children: <Widget>[
-          Container(
-            height: 50,
-            color: Theme.of(context).colorScheme.primary,
-            child: const Center(child: Text('Server 1 Placeholder')),
-          ),
-          const Padding(padding: EdgeInsets.all(5)),
-          Container(
-            height: 50,
-            color: Theme.of(context).colorScheme.primary,
-            child: const Center(child: Text('Server 2 Placeholder')),
-          ),
-          const Padding(padding: EdgeInsets.all(5)),
-          Container(
-            height: 50,
-            color: Theme.of(context).colorScheme.primary,
-            child: const Center(child: Text('Server 3 Placeholder')),
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: AnimatedList(
+              key: _key,
+              initialItemCount: 0,
+              padding: const EdgeInsets.all(10),
+              itemBuilder: (context, index, animation) {
+                return SizeTransition(
+                  key: UniqueKey(),
+                  sizeFactor: animation,
+                  child: Card(
+                    margin: const EdgeInsets.all(10),
+                    color: Colors.orangeAccent,
+                    child: ListTile(
+                      title: Text(
+                        _items[index],
+                        style: const TextStyle(fontSize: 24),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _removeItem(index);
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addItem,
+        child: const Icon(Icons.add),
+      ),
     );
+    // return Center(
+    //   child: ListView(
+    //     padding: const EdgeInsets.all(9),
+    //     children: <Widget>[
+    //       Container(
+    //         height: 50,
+    //         color: Theme.of(context).colorScheme.primary,
+    //         child: const Center(child: Text('Server 1 Placeholder')),
+    //       ),
+    //       const Padding(padding: EdgeInsets.all(5)),
+    //       Container(
+    //         height: 50,
+    //         color: Theme.of(context).colorScheme.primary,
+    //         child: const Center(child: Text('Server 2 Placeholder')),
+    //       ),
+    //       const Padding(padding: EdgeInsets.all(5)),
+    //       Container(
+    //         height: 50,
+    //         color: Theme.of(context).colorScheme.primary,
+    //         child: const Center(child: Text('Server 3 Placeholder')),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
