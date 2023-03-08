1597206f64cdbb3ba1b64ee9f6d6305c87c4708a
rwp005 <rwp005@gmail.com>
2023-03-08 14:31:51 -0600

    Added edit functionality

--- lib/widget/server_dialog.dart
+++ lib/widget/server_dialog.dart
@@ -3,15 +3,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uptime/providers/server_provider.dart';
 
 class ServerDialog extends ConsumerWidget {
-  ServerDialog({super.key});
-
+  ServerDialog({super.key, this.index});
+  final int? index; //? because we want to find nullable values
   final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final notifier = ref.watch(serversProvider);
+
+    index != null ? controller.text = notifier.items[index] : {};
+
     return AlertDialog(
-      title: const Text('New Server'),
+      title:
+          index != null ? const Text('Edit Server') : const Text('New Server'),
       content: SizedBox(
         height: 150,
         child: Column(
@@ -37,9 +41,11 @@ class ServerDialog extends ConsumerWidget {
           style: TextButton.styleFrom(
             textStyle: Theme.of(context).textTheme.labelLarge,
           ),
-          child: const Text('Add'),
+          child: index != null ? const Text('Edit') : const Text('Add'),
           onPressed: () {
-            notifier.addItem(controller.text);
+            index != null
+                ? notifier.editItem(controller.text, index)
+                : notifier.addItem(controller.text);
             Navigator.of(context).pop();
           },
         ),
