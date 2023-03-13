ea233bd30720f9b7f2632e9858f3fe51dc6a4d55
rwp005 <rwp005@gmail.com>
2023-03-13 14:53:06 -0500

    Working Select Statement

--- lib/screens/servers.dart
+++ lib/screens/servers.dart
@@ -12,100 +12,98 @@ class Servers extends ConsumerStatefulWidget {
 }
 
 class _ServersState extends ConsumerState<Servers> {
+  final Future<dynamic> response =
+      Supabase.instance.client.from('countries').select('name');
+
   @override
   Widget build(BuildContext context) {
     final notifier = ref.watch(serversProvider);
-    // return FutureBuilder(
-    //   future: Future._nullFuture,
-    //   builder: (context, snapshot) {
-    //     return Center(
-    //       child: Column(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         children: <Widget>[
-    //           Padding(
-    //             padding: const EdgeInsets.only(top: 16),
-    //             child: Text('Result: ${snapshot.data}'),
-    //           ),
-    //         ],
-    //       ),
-    //     );
-    //   },
-    // );
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: AnimatedList(
-              key: notifier.key,
-              initialItemCount: notifier.items.length,
-              padding: const EdgeInsets.all(10),
-              itemBuilder: (context, index, animation) {
-                return SizeTransition(
-                  key: UniqueKey(),
-                  sizeFactor: animation,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    color: Colors.orangeAccent,
-                    child: ListTile(
-                      //TODO add column widget to put in chart showing uptime for past X time
-
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90),
-                      ),
-                      title: SelectableText(
-                        notifier.items[index],
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          //TODO add color of tile basede on status up or down via last ping
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            // onPressed: () {
-                            //   showDialog<void>(
-                            //     context: context,
-                            //     builder: (BuildContext context) {
-                            //       return ServerDialog(index: index);
-                            //     },
-                            //   );
-                            // },
-                            onPressed: () async {
-                              notifier.blah = await Supabase.instance.client
-                                  .from('countries')
-                                  .select('name');
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {
-                              notifier.removeItem(index);
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
+    return FutureBuilder(
+      future: response,
+      builder: (context, snapshot) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Result: ${snapshot.data}'),
+              ),
+            ],
           ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return ServerDialog();
-            },
-          );
-        },
-        autofocus: true,
-        child: const Icon(Icons.add),
-      ),
+        );
+      },
     );
+    // return Scaffold(
+    //   body: Column(
+    //     children: [
+    //       Expanded(
+    //         child: AnimatedList(
+    //           key: notifier.key,
+    //           initialItemCount: notifier.items.length,
+    //           padding: const EdgeInsets.all(10),
+    //           itemBuilder: (context, index, animation) {
+    //             return SizeTransition(
+    //               key: UniqueKey(),
+    //               sizeFactor: animation,
+    //               child: Card(
+    //                 shape: RoundedRectangleBorder(
+    //                   borderRadius: BorderRadius.circular(20),
+    //                 ),
+    //                 color: Colors.orangeAccent,
+    //                 child: ListTile(
+    //                   //TODO add column widget to put in chart showing uptime for past X time
+
+    //                   shape: RoundedRectangleBorder(
+    //                     borderRadius: BorderRadius.circular(90),
+    //                   ),
+    //                   title: SelectableText(
+    //                     notifier.items[index],
+    //                     style: const TextStyle(fontSize: 20),
+    //                   ),
+    //                   trailing: Row(
+    //                     mainAxisSize: MainAxisSize.min,
+    //                     children: [
+    //                       //TODO add color of tile basede on status up or down via last ping
+    //                       IconButton(
+    //                         icon: const Icon(Icons.edit),
+    //                         onPressed: () {
+    //                           showDialog<void>(
+    //                             context: context,
+    //                             builder: (BuildContext context) {
+    //                               return ServerDialog(index: index);
+    //                             },
+    //                           );
+    //                         },
+    //                       ),
+    //                       IconButton(
+    //                         icon: const Icon(Icons.delete),
+    //                         onPressed: () {
+    //                           notifier.removeItem(index);
+    //                         },
+    //                       ),
+    //                     ],
+    //                   ),
+    //                 ),
+    //               ),
+    //             );
+    //           },
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: () {
+    //       showDialog<void>(
+    //         context: context,
+    //         builder: (BuildContext context) {
+    //           return ServerDialog();
+    //         },
+    //       );
+    //     },
+    //     autofocus: true,
+    //     child: const Icon(Icons.add),
+    //   ),
+    // );
   }
 }
