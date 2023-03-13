4425a0451b8403d4b87d8d0030f36c32de8d3a33
rwp005 <rwp005@gmail.com>
2023-03-13 15:14:40 -0500

    Select from Postgres with State

--- lib/screens/reports.dart
+++ lib/screens/reports.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:uptime/providers/server_provider.dart';
 
-class Reports extends StatefulWidget {
+class Reports extends ConsumerStatefulWidget {
   const Reports({super.key, required String title});
 
   @override
-  State<Reports> createState() => _ReportsState();
+  ConsumerState<Reports> createState() => _ReportsState();
 }
 
-class _ReportsState extends State<Reports> {
+class _ReportsState extends ConsumerState<Reports> {
   @override
   Widget build(BuildContext context) {
+    // final notifier = ref.watch(serversProvider);
     return const Placeholder();
   }
 }
